2359e068e9852c3b9904313575972924e02c1faa
A-Zalt <igor.a.omelchenko@gmail.com>
2026-02-20 17:12:22 +0200

    First commit

---
+++ include/gd/GJGameLevel.hpp
@@ -0,0 +1,6 @@
+#include <cocos2d.h>
+
+class GJGameLevel : public cocos2d::CCNode {
+public:
+    static GJGameLevel* create();
+};
---
+++ src/main.cc
@@ -0,0 +1,6 @@
+#include "modules/extension.cc"
+
+[[gnu::constructor]]
+int main() {
+    extension_init();
+}
